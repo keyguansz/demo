c31fbce770d6d53d438d78fc861fc68cd34d7f00
kgd <key.guan@qq.com>
2017-08-27 14:01:12 +0800

    clear the android dev tool

--- FitDemo/app/src/main/java/key/android/demo/FitDemo/activity/ZsTestActivity.java
+++ FitDemo/app/src/main/java/key/android/demo/FitDemo/activity/ZsTestActivity.java
@@ -1,7 +1,10 @@
 package key.android.demo.FitDemo.activity;
 
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import key.android.demo.FitDemo.R;
 
@@ -16,5 +19,17 @@ public class ZsTestActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_zs_test);
+        findViewById(R.id.test_img).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Drawable d = ((ImageView)findViewById(R.id.test_img)).getDrawable();
+                // normal:1920*1080:9.5M
+                // x:540*960:9.5M
+                //xx:360*640:2.5M
+                //  Log.e("ZsTestActivity","w="+d.getH)
+                int i = 0;
+            }
+        });
+
     }
 }
